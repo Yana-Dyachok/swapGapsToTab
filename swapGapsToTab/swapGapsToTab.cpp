a420f7b114d2c2653f4af65c7d69f40d1b47b0a8
Yana-Dyachok <97878430+Yana-Dyachok@users.noreply.github.com>
2022-02-05 13:49:42 +0200

    Add project files.

---
+++ swapGapsToTab/swapGapsToTab.cpp
@@ -0,0 +1,17 @@
+﻿// 1. Дан рядок символів. Замінити в ньому всі пробіли на табуляцію
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string Symbols;//створюємо об'єкт Symbols для типу string 
+	cout << "Enter any symbols with gaps" << endl;
+	getline(cin, Symbols);//вводимо дані в рядок
+	int size = Symbols.size();//створюємо розмір для даного рядка
+	for (int i = 0; i < size; i++)//створюємо цикл для заміни пробілів
+	{
+		if (Symbols[i] == ' ')//якщо у рядку є пробіли, то
+			Symbols[i] = '\t';//присвоюємо табуляцію замість пробілів
+	}
+	cout << Symbols<< endl;//виводимо дані - де пробіли замінені на табуляцію
+}
